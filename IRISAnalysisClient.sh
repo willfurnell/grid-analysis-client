89afccfb9f66c675e1cfb5970e6a6045c6ea54b6
Will Furnell <mail@willfurnell.com>
2016-02-28 11:32:26 +0000

    Updating library paths 1

--- IRISAnalysisClient.sh
+++ IRISAnalysisClient.sh
@@ -12,14 +12,26 @@
 #
 # This file has been modified from the original: https://github.com/gridpp/cvmfs-getting-started/blob/master/run.sh
 
-export PATH=/cvmfs/lucid.egi.eu/libraries/:$PATH
+export PATH=$1:/cvmfs/lucid.egi.eu/libraries/:$PATH
+
+echo "_____________________________"
+echo $PATH
+echo "_____________________________"
 
 # Add the Python modules from the LUCID CVMFS repo to the PYTHONPATH. This is so the software can use lucid-utils and
 # its dependencies.
 export PYTHONPATH=/cvmfs/lucid.egi.eu/libraries/python2.6/site-packages/:/cvmfs/lucid.egi.eu/libraries/:$PYTHONPATH
+echo $PYTHONPATH
+
+echo "_____________________________"
 
 # Add the supporting libraries to the library linking path.
-export LD_LIBRARY_PATH=/cvmfs/lucid.egi.eu/libraries/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/cvmfs/cernatschool.egi.eu/lib64:/cvmfs/lucid.egi.eu/libraries/:$LD_LIBRARY_PATH
+
+echo $LD_LIBRARY_PATH
+
+echo "_____________________________"
+
 
 # Run the Python script in the CVMFS directory.
 /usr/bin/python $1/analyse.py $2
